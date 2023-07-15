28f50cc52b21236b97ecfc50f81b900d735f626d
istiak <istieak.hasan1996@gmail.com>
2023-07-15 02:46:29 -0600

    implement authentication properly

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,24 @@
 import './App.css'
 import {RouterProvider} from 'react-router-dom'
 import routes from './routes/route'
+import { useAppDispatch } from './redux/hook';
+import {useEffect} from 'react'
+import { setLoading, setUser } from './redux/features/user/userSlice';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './lib/firebase';
 function App() {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(setLoading(true));
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(setUser(user.email as string));
+        dispatch(setLoading(false));
+      } else {
+        dispatch(setLoading(false));
+      }
+    });
+  }, [dispatch]);
 
   return (
     <>
--- src/shared/Navbar/Navbar.tsx
+++ src/shared/Navbar/Navbar.tsx
@@ -1,35 +1,71 @@
-
+import { signOut } from "firebase/auth";
+import { useAppDispatch, useAppSelector } from "../../redux/hook";
+import { auth } from "../../lib/firebase";
+import { setUser } from "../../redux/features/user/userSlice";
 
 const Navbar = () => {
-  
+  const { user } = useAppSelector((state) => state.user);
+  const dispatch = useAppDispatch();
+  const handleLogOut = () => {
+    signOut(auth).then(() => {
+      dispatch(setUser(null));
+    });
+  };
+  const routeLink = (
+    <>
+      <li>
+        <a>All Books</a>
+      </li>
+      {!user?.email ? (
+        <>
+          <li>
+            <a>Sign In</a>
+          </li>
+          <li>
+            <a>Sign Up</a>
+          </li>
+        </>
+      ) : (
+        <li onClick={handleLogOut}>
+          <a>Logout</a>
+        </li>
+      )}
+    </>
+  );
   return (
     <div className="navbar bg-[#27296d] text-white">
-    <div className="navbar-start">
-      <div className="dropdown">
-        <label tabIndex={0} className="btn btn-ghost lg:hidden">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-        </label>
-        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow  rounded-box w-52">
-          <li><a>All Books</a></li>
-          <li><a>Sign In</a></li>
-          <li><a>Sign Up</a></li>
-          <li><a>Logout</a></li>
-        </ul>
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow  rounded-box w-52"
+          >
+            {routeLink}
+          </ul>
+        </div>
+        <a className="btn btn-ghost normal-case text-xl">Car Fair</a>
       </div>
-      <a className="btn btn-ghost normal-case text-xl">Car Fair</a>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{routeLink}</ul>
+      </div>
+      <div className="navbar-end">{/* <a className="btn">Button</a> */}</div>
     </div>
-    <div className="navbar-center hidden lg:flex">
-      <ul className="menu menu-horizontal px-1">
-      <li><a>All Books</a></li>
-          <li><a>Sign In</a></li>
-          <li><a>Sign Up</a></li>
-          <li><a>Logout</a></li>
-      </ul>
-    </div>
-    <div className="navbar-end">
-      {/* <a className="btn">Button</a> */}
-    </div>
-  </div>
   );
 };
 
